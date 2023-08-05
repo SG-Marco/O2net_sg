4200f0f4910b13351fe804ac33987dc0d44cab45
SG <rollingpoe@gmai.com>
2023-08-05 16:09:50 +0900

    from ops.modules.ms_deform_attn import MSDeformAttn

--- O2net/models/deformable_transformer.py
+++ O2net/models/deformable_transformer.py
@@ -17,7 +17,7 @@ from torch import nn, Tensor
 from torch.nn.init import xavier_uniform_, constant_, uniform_, normal_
 
 from util.misc import inverse_sigmoid
-from ops.modules import MSDeformAttn
+from ops.modules.ms_deform_attn import MSDeformAttn
 
 
 class DeformableTransformer(nn.Module):
