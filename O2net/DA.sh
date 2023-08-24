fdfe2a7ec65f4b1e92ee41c4bb762f62003e4cea
SG <rollingpoe@gmai.com>
2023-08-25 04:40:09 +0900

    update da.sh path to google drive

--- O2net/DA.sh
+++ O2net/DA.sh
@@ -1,7 +1,7 @@
 # pre-training the source model
-CUDA_VISIBLE_DEVICES=* GPUS_PER_NODE=n ./tools/run_dist_launch.sh n ./configs/r50_deformable_detr.sh --output_dir exps/source_model --dataset_file city2foggy_source
+CUDA_VISIBLE_DEVICES=0 GPUS_PER_NODE=1 ./tools/run_dist_launch.sh 1 ./configs/r50_deformable_detr.sh --output_dir exps/source_model --dataset_file city2foggy_source --coco_path /content/drive/MyDrive/Cityscapes_data/data
 
 # training the proposed method
-CUDA_VISIBLE_DEVICES=* GPUS_PER_NODE=n ./tools/run_dist_launch.sh n ./configs/DA_r50_deformable_detr.sh --output_dir exps/ours --transform make_da_transforms --dataset_file city2foggy --checkpoint exps/source_model/checkpoint.pth
+CUDA_VISIBLE_DEVICES=0 GPUS_PER_NODE=1 ./tools/run_dist_launch.sh 1 ./configs/DA_r50_deformable_detr.sh --output_dir exps/ours --transform make_da_transforms --dataset_file city2foggy --checkpoint --coco_path /content/drive/MyDrive/Cityscapes_data/data/l2_norm/checkpoint.pth --coco_path /content/drive/MyDrive/Cityscapes_data/data
 
 
